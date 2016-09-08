98deda084b79b65310b3e9c70ffca58febd6170b
Devesh Shetty <devesh.shetty007@gmail.com>
2016-09-09 00:13:38 +0530

    Fixed some issues with integrating butterKnife

--- app/src/main/java/task/shopify/www/shopifytask/MainActivity.java
+++ app/src/main/java/task/shopify/www/shopifytask/MainActivity.java
@@ -2,12 +2,21 @@ package task.shopify.www.shopifytask;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
 public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.tv_total_cost)TextView tvTotalCost;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        
     }
 }
