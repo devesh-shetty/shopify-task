4ea3bf61c68b217f86b1da511d66911f7bec3ff8
Devesh Shetty <devesh.shetty007@gmail.com>
2016-09-08 18:03:06 +0530

    Initial app

---
+++ app/src/main/java/task/shopify/www/shopifytask/MainActivity.java
@@ -0,0 +1,13 @@
+package task.shopify.www.shopifytask;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
